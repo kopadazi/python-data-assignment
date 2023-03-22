263aef0e8d9a48ef4db1ea6b1cc2053a52b3c8f4
kopadazi <128495467+kopadazi@users.noreply.github.com>
2023-03-22 12:26:51 +0200

    Add files via upload

---
+++ pydata assignment.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+data=pd.read_csv('finance_liquor_sales.csv')
+grp_data=data.groupby(['zip_code','store_name'])
+perc_sales=grp_data['sale_dollars'].sum()/data['sale_dollars'].sum()
+most_popular=grp_data['item_description'].max()[perc_sales.idxmax()]
+print(most_popular,perc_sales)
+import matplotlib.pyplot as plt
+x=data['zip_code']
+y=data['bottles_sold']
+plt.scatter(x,y)
+plt.xlabel('Zip Code')
+plt.ylabel('Bottles Sold')
+plt.title('Bottles Sold')
+plt.show()
